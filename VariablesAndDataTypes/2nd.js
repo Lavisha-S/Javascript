e0e274163cb34a245e05a0389fce48d646751efb
Lavisha S <work.lavishaa@gmail.com>
2024-08-03 07:30:09 +0000

    commit

---
+++ VariablesAndDataTypes/2nd.js
@@ -0,0 +1,7 @@
+let num=prompt("enter a number:");
+if(num%3===0){
+    console.log(num,"is multiple of 3")
+
+}else{
+    console.log(num,"is not multiple of 3")
+}
---
+++ VariablesAndDataTypes/first.js
@@ -0,0 +1,42 @@
+console.log("Hello World"); 
+fullName="Tony Stark";
+
+price=99.99;
+radius:14;
+//a=null;
+y=undefined;
+isFollow=true;
+fullName=25;
+console.log(fullName)
+const student={
+    fullName:"Rahul",
+    age:20,
+    cgpa:8.2,
+    isPass:true
+};
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+let a=9;
+let b=5;
+console.log("a=",a,"&b=",b);
+console.log("a+b=",a+b);
+console.log("a-b=",a-b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b);
+//conditional statements
+let mode="dark"
+let color;
+if(mode==="dark"){
+    color="black";
+}
+if(mode==="light"){
+    color="white";
+}
+console.log(color)
+if(mode==="dark") console.log(mode);
+//ternary operator
+let age=16;
+let result=age>=18?"adult":"not adult"
+console.log(result);
